99c1f5612e88727cdf7ba7339a38ba593fd23fa2
KammaMahendra <babumahendra604@gmail.com>
2024-07-08 07:56:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Quiz from './quiz.js';
+
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <Quiz />
+            </header>
+        </div>
+    );
+}
+
+export default App;
+
+
